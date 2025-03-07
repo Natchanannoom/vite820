844393ec13a0ce06ed46c53d27809f90747eb1ff
natchanan <natchanan.no@ku.th>
2025-03-07 16:11:09 +0700

    edit git

--- src/sections/Experience/index.jsx
+++ src/sections/Experience/index.jsx
@@ -1,5 +1,6 @@
 import { faGithub, faYoutube ,faMedium} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import picExp from "../../assets/picExp1.png";
 const Experience = () => {
     return (
         <div className="space-y-4">
@@ -7,9 +8,14 @@ const Experience = () => {
         <div className="grid grid-cols-[25%_75%]">
 
             <div>
-            <div>2024-2025</div>
-            <div>image</div>
+                <div className="text-left">
+                    <span className="text-sm">2024-2025</span>
+                </div>
+                <div>
+            <img src={picExp} className="w-5/6 rounded-md border-2" />
         </div>
+        </div>
+
 
         <div className="flex flex-col gap-y-4">
             <div className="text-primaryAccent">Fashion</div>
@@ -20,8 +26,8 @@ const Experience = () => {
             </div>
             <div className="text-left text-sm">Design
             <div className="text-left flex gap-4 text-sm">
-                <div className="text-gray-100 font-medium bg-primaryTitle px-2 py-1 rounded-md">React</div>
-                <div className="text-gray-100 font-medium  bg-primaryTitle px-2 py-1 rounded-md">Tailwind</div>
+                <div className="text-primaryBase font-medium bg-primaryTitle px-2 py-1 rounded-md hover:text-gray-100">React</div>
+                <div className="text-primaryBase font-medium  bg-primaryTitle px-2 py-1 rounded-md  hover:text-gray-100">Tailwind</div>
 
         </div>
     </div>
