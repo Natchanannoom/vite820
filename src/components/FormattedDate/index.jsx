7ad3640e7f39d98d4f2d9f6b2a507d3b8f6c21e4
natchanan <natchanan.no@ku.th>
2025-03-08 15:58:52 +0700

    edit git

---
+++ src/components/FormattedDate/index.jsx
@@ -0,0 +1,10 @@
+const FormattedDate = ({ isHightlight,children }) => {
+    return (
+        <div>
+             <span className={`text-sm ${isHightlight? "text-primaryAccent" : ""}`}>
+             {children}
+             </span>
+        </div>
+    )
+    }
+export default FormattedDate;
